61383e82f23d414c1ef724162aeb35ff2f2e807a
Brett Hazen <BHAZEN@beckman.com>
2021-11-03 09:06:08 -0500

    Updated to V2 of durable functions

--- src/DurableFunctionsDemo/DurableFunctionsDemo/ApprovalRequestResponse.cs
+++ src/DurableFunctionsDemo/DurableFunctionsDemo/ApprovalRequestResponse.cs
@@ -1,14 +1,10 @@
-using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using System.Net.Http;
 using DurableFunctionsDemo.Models;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 
 namespace DurableFunctionsDemo
 {
@@ -17,7 +13,7 @@ namespace DurableFunctionsDemo
         [FunctionName(nameof(ApprovalRequestResponseHttp))]
         public async Task<IActionResult> ApprovalRequestResponseHttp(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "approval")] HttpRequestMessage req,
-            [OrchestrationClient] DurableOrchestrationClient orchestrationClient)
+            [DurableClient] IDurableOrchestrationClient orchestrationClient)
         {
             var response = await req.Content.ReadAsAsync<ApprovalResponse>();
 
